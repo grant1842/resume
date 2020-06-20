a0f9d431b9b3cf1631674a04fcbe01de829b55c1
Grant Fullen <gfullen@stellarstudios.com>
2020-06-20 08:32:54 -0400

    fix viewport height on mobile menu

--- components/layout.tsx
+++ components/layout.tsx
@@ -24,7 +24,11 @@ const Layout = ({ children, headChildren, pageTitle = 'Grant Fullen - React Resu
     return (
         <>
             <Meta />
-            <div className='flex flex-col min-h-screen'>
+            <div
+                className={`flex flex-col ${
+                    mobileMenuVisible ? 'h-screen overflow-hidden' : 'min-h-screen'
+                } md:overflow-auto`}
+            >
                 {/* <Alert preview={preview} /> */}
                 <main className={'flex-1 flex flex-col'}>
                     <Head>
